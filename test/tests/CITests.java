c60db3e8b194cc84c615e3b8831140ff715b2171
Fabian Steeg <steeg@hbz-nrw.de>
2022-03-08 17:55:36 +0100

    Update CI test setup (RPB-8)

--- test/tests/CITests.java
+++ test/tests/CITests.java
@@ -4,6 +4,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import rpb.DecodeTest;
+import rpb.EtlTest;
+
 /**
  * All quick, self-contained tests for running as CI. For running all
  * tests, including long running tests and integration tests with dependencies
@@ -13,7 +16,7 @@ import org.junit.runners.Suite.SuiteClasses;
  *
  */
 @RunWith(Suite.class)
-@SuiteClasses({ ApplicationTest.class, InternalIntegrationTest.class })
+@SuiteClasses({ ApplicationTest.class, /*InternalIntegrationTest.class,*/ DecodeTest.class, EtlTest.class})
 public class CITests {
 	//
 }

8b525f8348069dae304bd33eddaed6b481702ac9
Fabian Steeg <steeg@hbz-nrw.de>
2023-10-26 17:12:33 +0200

    Use daily Allegro dump in transformAndIndex.sh script (RPB-101)
    Add extracted files to .gitignore, backup ZIP with timestamp

--- transformAndIndex.sh
+++ transformAndIndex.sh
@@ -2,10 +2,19 @@
 set -eu
 IFS=$'\n\t'
 
+# Get the daily Allegro dump:
+cd conf
+wget https://rpb.lbz-rlp.de/rpb04/intern/RPBEXP.ZIP
+unzip -o RPBEXP.ZIP
+mv RPBEXP.ZIP RPBEXP/RPBEXP-$(date "+%Y%m%d-%H%M").ZIP
+cd ..
+
+# Transform the data:
 sbt "runMain rpb.ETL conf/rpb-sw.flux"
 sbt "runMain rpb.ETL conf/rpb-titel-to-strapi.flux"
 sbt "runMain rpb.ETL conf/rpb-titel-to-lobid.flux"
 
+# Index to Elasticsearch:
 unset http_proxy # for posting to weywot3
 for filename in conf/output/bulk/bulk-*.ndjson
 do

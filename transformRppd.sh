e6a868588250266f5ba3ba00036ccf2262626815
Fabian Steeg <steeg@hbz-nrw.de>
2023-11-28 14:00:56 +0100

    Use Beacon-based lookup files in RPPD transformation (RPB-38)

---
+++ transformRppd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eu
+
+bash transformBeacons.sh
+rm conf/output/bulk/rppd/*
+sbt "runMain rpb.ETL conf/rppd-to-strapi.flux IN_FILE=RPB-Export_HBZ_Bio.txt OUT_FILE=output-rppd-strapi.ndjson"
+sbt "runMain rpb.ETL conf/rppd-to-lobid.flux"
+
+# Indexing happens in rppd/transformAndIndexRppd.sh (lobid-gnd repo, branch 'rppd'), which calls this script

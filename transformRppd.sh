0500bae8e563a6064cd47f29c9f0f7c2cd02f545
Fabian Steeg <steeg@hbz-nrw.de>
2024-03-13 16:29:40 +0100

    Support strapi export data in rppd-to-lobid workflows (RPB-59)

--- transformRppd.sh
+++ transformRppd.sh
@@ -7,5 +7,8 @@ sbt "runMain rpb.ETL conf/rppd-to-strapi.flux IN_FILE=RPB-Export_HBZ_Bio.txt OUT
 sbt "runMain rpb.ETL conf/rppd-to-gnd-mapping.flux"
 sbt "runMain rpb.ETL conf/rppd-rppdId-with-label-map.flux"
 sbt "runMain rpb.ETL conf/rppd-to-lobid.flux"
+# Or from Strapi export:
+# zgrep -a '"type":"api::person.person"' conf/strapi-export.tar.gz > conf/output/rppd-export.jsonl
+# sbt "runMain rpb.ETL conf/rppd-to-lobid.flux IN_FILE=rppd-export.jsonl RECORD_PATH=data"
 
 # Indexing happens in rppd/transformAndIndexRppd.sh (lobid-gnd repo, branch 'rppd'), which calls this script

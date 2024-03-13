f6d9faf5bfe46a68f44a28f7ca5252317074ac34
Fabian Steeg <steeg@hbz-nrw.de>
2024-03-13 16:37:39 +0100

    Create lookup tables before running both transformations (RPB-59)
    Some lookups were recently moved to the to-strapi transformation

--- transformRppd.sh
+++ transformRppd.sh
@@ -3,9 +3,9 @@ set -u
 
 bash transformBeacons.sh
 rm conf/output/bulk/rppd/*
-sbt "runMain rpb.ETL conf/rppd-to-strapi.flux IN_FILE=RPB-Export_HBZ_Bio.txt OUT_FILE=output-rppd-strapi.ndjson"
 sbt "runMain rpb.ETL conf/rppd-to-gnd-mapping.flux"
 sbt "runMain rpb.ETL conf/rppd-rppdId-with-label-map.flux"
+sbt "runMain rpb.ETL conf/rppd-to-strapi.flux IN_FILE=RPB-Export_HBZ_Bio.txt OUT_FILE=output-rppd-strapi.ndjson"
 sbt "runMain rpb.ETL conf/rppd-to-lobid.flux"
 # Or from Strapi export:
 # zgrep -a '"type":"api::person.person"' conf/strapi-export.tar.gz > conf/output/rppd-export.jsonl

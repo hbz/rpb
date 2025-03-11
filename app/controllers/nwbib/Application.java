7375618aceef70925c9465f05077e090253b5951
Fabian Steeg <steeg@hbz-nrw.de>
2025-03-11 11:09:28 +0100

    Don't include `describedBy` from lobid record (RPB-251)

--- app/controllers/nwbib/Application.java
+++ app/controllers/nwbib/Application.java
@@ -1028,6 +1028,7 @@ public class Application extends Controller {
 		MapType mapType = TypeFactory.defaultInstance().constructMapType(Map.class, String.class, Object.class);
 		Map<String, Object> transformedMap = objectMapper.readValue(transformedJson.toString(), mapType);
 		Map<String, Object> lobidMap = objectMapper.readValue(lobidJson.toString(), mapType);
+		lobidMap.remove("describedBy");
 		transformedMap.remove("type");
 		transformedMap.keySet().forEach(key -> {
 			Object transformedObject = transformedMap.get(key);

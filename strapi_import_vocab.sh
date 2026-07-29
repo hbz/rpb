5a47215f9934074befb066fc9b0bf16c1eda3887
Phu Tu <phu.tu@hbz-nrw.de>
2026-07-29 12:34:14 +0200

    Use test branch (not dev)
    see https://github.com/hbz/lbz-vocabs/issues/13

--- strapi_import_vocab.sh
+++ strapi_import_vocab.sh
@@ -16,7 +16,7 @@ VOCABS_REPO=/home/sol/git/lbz-vocabs
 
 cd "$VOCABS_REPO"
 
-git switch dev
+git switch test
 
 # get changes from remote
 git fetch origin
@@ -78,8 +78,8 @@ done <<< "$changed_files"
 
 #
 # Change back to lbz-vocabs repo and
-# set/update local dev branch from remote
+# set/update local test branch from remote
 #
 
-cd "$VOCABS_REPO" && git reset --hard origin/dev
+cd "$VOCABS_REPO" && git reset --hard origin/test
 

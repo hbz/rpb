e9cde6daccb1238026abe6a3345f3349e703aef5
Fabian Steeg <steeg@hbz-nrw.de>
2026-09-02 13:08:17 +0200

    Use our backup exports for RPPD indexing, update script (RPB-55)
    Strapi export contains no `depiction` data for media file links

--- transformRppd.sh
+++ transformRppd.sh
@@ -7,11 +7,11 @@ rm etl/output/bulk/rppd/*
 # Here, we used to import Allegro data:
 # sbt --java-home $JAVA_HOME "runMain rpb.ETL etl/rppd-to-strapi.flux IN_FILE=RPB-Export_HBZ_Bio.txt OUT_FILE=output-rppd-strapi.ndjson"
 # sbt --java-home $JAVA_HOME "runMain rpb.ETL etl/rppd-to-lobid.flux"
-# But now we use the Strapi export:
-zgrep -a '"type":"api::person.person"' etl/strapi-export.tar.gz > etl/output/rppd-export.jsonl
+# Now we use the Strapi export (but contains no relations, e.g. depiction details):
+zgrep -a '"type":"api::person.person","id":' etl/strapi-export.tar.gz > etl/output/rppd-export.jsonl
+# Then, we add the full backup exports (like for the title data), to include depictions:
+cat etl/persons.ndjson | grep '"data"' >> etl/output/rppd-export.jsonl
 cp etl/output/rppd-export.jsonl ../rppd/conf/ # used in rppd for robots.txt
-sbt --java-home $JAVA_HOME "runMain rpb.ETL etl/rppd-to-gnd-mapping.flux"
-sbt --java-home $JAVA_HOME "runMain rpb.ETL etl/rppd-rppdId-with-label-map.flux"
 sbt --java-home $JAVA_HOME -mem 3000 "runMain rpb.ETL etl/rppd-to-lobid.flux IN_FILE=rppd-export.jsonl RECORD_PATH=data"
 
 # Indexing happens in rppd/transformAndIndexRppd.sh (lobid-gnd repo, branch 'rppd'), which calls this script
